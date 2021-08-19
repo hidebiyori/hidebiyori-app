1dd7a9d7a7250d4b579780c7f171aff2a052254e
hidebiyori <87452612+hidebiyori@users.noreply.github.com>
2021-08-19 22:36:56 +0900

    remove the leading hash (#) from the URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  setPathUrlStrategy();
   runApp(MyApp(firestore: firestore));
 }
 
